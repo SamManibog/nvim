7c1550ce732d9972e00fc0555ce71c1f24d2f2cd
SamManibog <sfmanibog@outlook.com>
2025-03-14 21:19:50 -1000

    opening vim fugitive now also opens it in its own tabpage

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,17 @@ return {
     "tpope/vim-fugitive",
     lazy = false,
     config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gs", function()
+            vim.cmd("tab split")
+            vim.cmd("Git")
+            local git_win = vim.fn.win_getid()
+            local sibling_ids = vim.api.nvim_tabpage_list_wins(0)
+            for _, id in pairs(sibling_ids) do
+                print(id)
+                if id ~= git_win then
+                    vim.api.nvim_win_close(id, false)
+                end
+            end
+        end)
     end
 }
