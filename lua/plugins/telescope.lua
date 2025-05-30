4d0f05bf6460a106b3efa94b4dee27e5ecfbf3d6
Sam Manibog <sfmanibog@outlook.com>
2025-05-29 15:27:34 -1000

    configured telescope to search hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,22 @@ return {
     lazy = false,
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', function()
+            local search_dir = nil
+            local first_file = vim.fn.argv()[1]
+
+            if first_file ~= nil then
+                local first_entry = vim.uv.fs_stat(first_file)
+                if first_entry ~= nil and first_entry.type == "directory" then
+                    search_dir = first_file
+                end
+            end
+
+            builtin.find_files({
+                cwd = search_dir,
+                hidden = true,
+            })
+        end)
         vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({search = vim.fn.input("Grep > ")})
