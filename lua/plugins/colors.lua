c6ee3fe95433cd32804d563a3cf6aaeabac618ae
Sam <sfmanibog@outlook.com>
2025-06-14 10:31:53 -1000

    edited gruvbox config

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,32 +1,35 @@
 return {
-    { 
+    {
         "ellisonleao/gruvbox.nvim",
 		lazy = false,
         priority = 1000,
-        config = true,
-        opts = {
-            terminal_colors = true, -- add neovim terminal colors
-            undercurl = true,
-            underline = true,
-            bold = true,
-            italic = {
-                strings = false,
-                emphasis = true,
-                comments = true,
-                operators = false,
-                folds = true,
-            },
-            strikethrough = true,
-            invert_selection = false,
-            invert_signs = false,
-            invert_tabline = false,
-            inverse = false, -- invert background for search, diffs, statuslines and errors
-            contrast = "soft", -- can be "hard", "soft" or empty string
-            palette_overrides = {},
-            overrides = {},
-            dim_inactive = false,
-            transparent_mode = false,
-        }
+        config = function()
+            require("gruvbox").setup({
+                terminal_colors = true, -- add neovim terminal colors
+                undercurl = true,
+                underline = true,
+                bold = true,
+                italic = {
+                    strings = false,
+                    emphasis = true,
+                    comments = true,
+                    operators = false,
+                    folds = true,
+                },
+                strikethrough = true,
+                invert_selection = false,
+                invert_signs = false,
+                invert_tabline = false,
+                inverse = true, -- invert background for search, diffs, statuslines and errors
+                contrast = "soft", -- can be "hard", "soft" or empty string
+                palette_overrides = {},
+                overrides = {
+                    ["@namespace"] = { link = "Type" }
+                },
+                dim_inactive = false,
+                transparent_mode = false,
+            })
+        end
     },
 	{
 		"rebelot/kanagawa.nvim",
